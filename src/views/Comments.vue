f91b4b78cb0d861522cd18742d4ee5dc6af0e248
Djajusman <tsaniyadiriyadh@gmail.com>
2022-02-09 06:18:03 +0700

    update

--- src/views/Comments.vue
+++ src/views/Comments.vue
@@ -1,97 +1,55 @@
 <template>
-  <div class="root">
-    <div class="wrapper-main">
-      <div class="left-align">
-        <h2 class="bold center" style="color: #3aac5d">To Do list in Vue.js</h2>
-      </div>
-      <div v-for="item in comments" :key="item.id">
-        <p class="fs-15 grey-label" style="margin-bottom: 0px">
-          {{ item.name }}
-        </p>
-        <p>
-          {{ item.content }}
-          <span
-            class="right fs-12 grey-label pointer"
-            @click="clicklike(item.id)"
-          >
-            Like {{ item.like }}
-          </span>
-        </p>
-      </div>
+  <div id="app" class="container">
+    <h1>Comment List</h1>
+
+    <div v-if="!isEditing">
+      <input type="text" v-model="comment" />
+      <input type="submit" value="Add" @click="storeComment" />
     </div>
-    <label class="block fs-15 grey-label">name</label>
-    <input
-      type="text"
-      v-model="newComment.name"
-      style="border: 1px solid #73c000; border-radius: 4px; margin-bottom: 20px"
-    />
-    <label class="block fs-15 grey-label">Comment</label>
-    <textarea
-      v-model="newComment.content"
-      v-on:keyup.enter="addComment()"
-      class="textarea"
-    ></textarea>
+    <div v-else>
+      <input type="text" v-model="comment" />
+      <input type="submit" value="Update" @click="updateComment" />
+    </div>
+    <ol>
+      <li v-for="(comment, index) in comments" :key="comment.id">
+        {{ comment }}
+        <button @click="editComment(index, comment)">Edit</button>
+        <button @click="removeComment(index)">Delete</button>
+      </li>
+    </ol>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      likeid: 0,
-      like: false,
-      comments: [
-        {
-          name: 'lucy',
-          content: 'Hi, edward wanna play?',
-          time: '29 minutes ago',
-          like: 2
-        },
-        {
-          name: 'edward',
-          content: 'Yes, I want to join to play',
-          time: '27 minutes ago',
-          like: 7
-        },
-        {
-          name: 'lucy',
-          content: 'When do you have free time',
-          time: '26 minutes ago',
-          like: 1
-        }
-      ],
-      newComment: {
-        name: '',
-        content: ''
+      data: {
+        isEditing: false,
+        comment: '',
+        comments: [],
+        selectedComment: null
       },
+
       methods: {
-        clicklike(id) {
-          this.like = true
-          this.likeid = id
+        storeComment() {
+          this.comments.push(this.comment)
+          this.comment = ''
         },
-        addComment() {
-          this.comments.push({
-            id: 4,
-            name: this.newComment.name,
-            content: this.newComment.content,
-            time: new Date(),
-            like: 0
-          })
-          this.newComment.name = ' '
-          this.newComment.content = ' '
-        }
-      },
-      computed: {
-        liked: function () {
-          if (this.like) {
-            for (var j = 0; j < this.comments.length; j++) {
-              if (this.comments[j].id == this.likeid) {
-                return this.comments[j].like + 1
-                // break
-              }
-            }
-          } else {
-            this.comments.like
-          }
+
+        removeComment(index) {
+          this.comments.splice(index, 1)
+        },
+
+        updateComment() {
+          this.comments.splice(this.selectedIndex, 1, this.comment)
+          this.comment = ''
+          this.isEditing = false
+        },
+
+        editComment(index, comment) {
+          this.isEditing = true
+          this.comment = comment
+          this.selectedIndex = index
         }
       }
     }
